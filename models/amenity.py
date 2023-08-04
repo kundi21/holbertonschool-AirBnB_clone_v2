700034a58c2b7d757b68518bc2d440f43fbd9f76
kundi21 <cundi.machado@gmail.com>
2023-08-04 14:13:30 -0700

    added relationship with places

--- models/amenity.py
+++ models/amenity.py
@@ -4,14 +4,15 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
 import os
-
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "amenities"
 
-        name = Column(String(128), unique=True,
-                      primary_key=True, nullable=False)
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary='place_amenity',
+                                   viewonly=False)
     else:
         name = ""
