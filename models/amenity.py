80b09985ea5a16e14c1a3ed2dc3c1b2f77e17328
kundi21 <cundi.machado@gmail.com>
2023-08-04 14:29:22 -0700

    broke another thing, trying something

--- models/amenity.py
+++ models/amenity.py
@@ -3,16 +3,16 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
-from os import getenv
+import os 
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
-
-    __tablename__ = "amenities"
-
-    name = Column(String(128), unique= nullable=False)
     
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+
+        __tablename__ = "amenities"
+
+        name = Column(String(128), unique= nullable=False)
         
         place_amenities = relationship("Place", secondary='place_amenity',
                                    viewonly=False)
