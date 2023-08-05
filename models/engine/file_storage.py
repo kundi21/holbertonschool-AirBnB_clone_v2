9f629a4651f6a6fde5cfaac4817119a1ddaefc12
TheWhiteMachine <priveiro@gmail.com>
2023-08-05 11:51:59 -0700

    new all in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,8 +15,8 @@ class FileStorage:
         else:
             list_of_obj = {}
             for key, val in self.__objects.items():
-                if FileStorage.__objects[key].__class__ == cls:
-                    list_of_obj[key] = str(val)
+                if cls == val.__class__ or cls == val.__class__.__name__:
+                    list_of_obj[key] = val
             return list_of_obj
 
     def new(self, obj):
