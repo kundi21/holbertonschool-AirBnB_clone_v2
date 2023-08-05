b6734db731b04280b1416aed3bd23fa8fd7c7616
kundi21 <cundi.machado@gmail.com>
2023-08-04 18:00:10 -0700

    fixing

--- models/user.py
+++ models/user.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" This module defines a class User"""
-import models
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes """
-    
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
+    """This class defines a user by various attributes"""
+
+    __tablename__ = "users"
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), unique=True, nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        first_name = Column(String(128), unique=True, nullable=True)
+        last_name = Column(String(128), unique=True, nullable=True)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        # reviews = relationship("Review", backref="user",
+        #                        cascade="all, delete, delete-orphan")
     else:
+        id = ""
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+        places = ""
+        reviews = ""
