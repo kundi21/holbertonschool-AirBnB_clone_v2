ad1366bfa34748ee8afcfb8d19dae7a9ed851e04
kundi21 <cundi.machado@gmail.com>
2023-08-06 18:44:26 -0700

    another tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,6 +6,9 @@ from typing import Any
 import unittest
 from console import HBNBCommand
 from models.engine.file_storage import FileStorage
+from contextlib import redirect_stdout
+import io
+
 
 
 class TestConsole(unittest.TestCase):
@@ -27,3 +30,23 @@ class TestConsole(unittest.TestCase):
     def test_EOF(self):
         """Test EOF"""
         self.assertEqual(self.console.onecmd("EOF"), None)
+    
+class TestYourClass(unittest.TestCase):
+    def test_create(self):
+        state_name = "California"
+        
+        with io.StringIO() as state_output, redirect_stdout(state_output):
+            self.console.onecmd(f"create State name='{state_name}'")
+            state_id = state_output.getvalue()
+
+        with io.StringIO() as city_output, redirect_stdout(city_output):
+            self.console.onecmd(
+                "create City state_id='{}' name='San_Francisco_is_super_cool'".format(state_id))
+            city_id = city_output.getvalue()
+
+        print("State ID: {}, City ID: {}".format(state_id, city_id))
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
